35d28d7a887cc297d2590d6f29e5abf282f9dab6
habib <habibdev96@gmail.com>
2021-06-22 18:23:47 -0600

    added aos

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
+import AOS from 'aos';
 import preloader from './preloader.js';
 
 const init = () => {
   preloader();
+  AOS.init({ offset: 100, duration: 1000, once: true });
 };
 
 window.addEventListener('DOMContentLoaded', init);
